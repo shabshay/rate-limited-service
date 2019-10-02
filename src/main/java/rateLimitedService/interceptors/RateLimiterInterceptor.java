0eeb496f9b8cbabdb28dd2a884472935cf9ba7d6
shay shabtay <shay.shabshay@gmail.com>
2019-10-02 20:20:32 +0300

    use new rate limiter

--- src/main/java/rateLimitedService/interceptors/RateLimiterInterceptor.java
+++ src/main/java/rateLimitedService/interceptors/RateLimiterInterceptor.java
@@ -1,24 +1,31 @@
 package rateLimitedService.interceptors;
 
-import es.moki.ratelimitj.core.limiter.concurrent.ConcurrentLimitRule;
-import es.moki.ratelimitj.inmemory.concurrent.InMemoryConcurrentRequestRateLimiter;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import rateLimiter.IRateLimiter;
+import rateLimiter.InMemory.InMemoryRateLimiterBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Interceptor that will validate the rate limitation by using the in memory rate limiter
+ */
 @Component
 public class RateLimiterInterceptor implements HandlerInterceptor {
 
     private final static Logger logger = Logger.getLogger(RateLimiterInterceptor.class);
-    private static InMemoryConcurrentRequestRateLimiter limiter;
+    private static IRateLimiter<String> limiter;
 
-    public static void initialize(int requestsLimit, int seconds){
-        limiter = new InMemoryConcurrentRequestRateLimiter(
-                ConcurrentLimitRule.of(requestsLimit, TimeUnit.SECONDS, seconds));
+    public static void initialize(int requestsLimit, int seconds) {
+        limiter = InMemoryRateLimiterBuilder
+                .newBuilder()
+                .setRateLimit(requestsLimit)
+                .setIntervalTime(seconds)
+                .setTimeUnit(TimeUnit.SECONDS)
+                .build();
     }
 
     @Override
@@ -29,13 +36,18 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
 
         logger.debug(String.format("Checking rate limit for user: %s", clientId));
 
-        if(clientId == null || clientId.isEmpty()){
+        if (clientId == null || clientId.isEmpty()) {
             logger.warn(String.format("Invalid client id, returning bad request", clientId));
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return false;
         }
 
-        if (!limiter.acquire(clientId).hasAcquired()) {
+        var result = limiter.acquire(clientId);
+        logger.debug("[Client - " + clientId + "] Acquire result: " + result.isSucceeded() +
+                ", expiration time: " + result.getExpirationTime().getTime().toString() +
+                ", remaining hits: " + result.getRemainHits());
+
+        if (!result.isSucceeded()) {
             logger.warn(String.format("User %s has exceeded the rate limit, returning service unavailable", clientId));
             response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
             return false;
